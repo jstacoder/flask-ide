518280807b6f0eeb9be83cc832ae9ad40650f678
kyle roux <kyle@level2designs.com>
2015-01-30 20:37:19 -0800

    locals

--- flask_ide/local_settings.py
+++ flask_ide/local_settings.py
@@ -2,4 +2,4 @@ import os
 
 class LocalConfig(object):
     SECRET_KEY = 'A Secret Shhh'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRES_URI',None) or 'sqlite:///test_ee.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',None) or 'sqlite:///test_ee.db'
