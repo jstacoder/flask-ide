2b406f176455412a2f9739ed7f22c3de7371982c
kyle roux <kyle@level2designs.com>
2015-02-25 21:08:57 -0800

    db

--- show_db_details.py
+++ show_db_details.py
@@ -2,4 +2,4 @@ from flask_ide import app,core
 
 with app.app_context():
     cls = core.models.ConnectionRecord
-    print '\n'.join(map(str,[x.ip_address for x in cls.query.all()]))
+    print '\n'.join(map(str,[(x.ip_address,x.date) for x in cls.query.all()]))
