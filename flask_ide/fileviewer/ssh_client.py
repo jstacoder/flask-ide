e9ecd12eee7f8754f34f22b02150b1d7fcd10b07
kyle roux <kyle@level2designs.com>
2015-08-06 16:59:10 -0700

    fixing dir and file handling via ssh

--- flask_ide/fileviewer/ssh_client.py
+++ flask_ide/fileviewer/ssh_client.py
@@ -2,6 +2,8 @@
 import paramiko
 import sys
 import json
+import stat
+import os
 from time import sleep
 
 
@@ -23,14 +25,12 @@ class SFTPConnection(object):
         self.kwargs = kwargs
         self._c = self.sftp
 
-    @property
     def conn(self):
-        rtn = login(self._user,self._password,self._host)
-        return rtn
+        return login(self._user,self._password,self._host)
 
     @property
     def sftp(self):
-        self._conn = self.conn
+        self._conn = self.conn()
         self._sftp = self._conn.open_sftp()
         return self._sftp
 
@@ -43,50 +43,57 @@ class SFTPConnection(object):
             return locals()[name]
         return None
 
-    def is_file(self,f,search_files=True,hidden=False):
+    def _get_mode(self,fname):
+        cmd = "stat --format=%f {}".format(fname)
+        #print 'executing',cmd
+        si,so,se = self._conn.exec_command(cmd)
+        data = so.read().strip()
+        #print 'got',data
+        result = data and int(data,16)
+        return result
+
+    def _get_real_path(self,fname):
+        cmd = 'python -c "import os; print os.path.realpath('+repr(fname)+')"'
+        si,so,se = self._conn.exec_command(cmd)
+        return so.read().strip()
+        
+    def _get_real_mode(self,fname,search_files=True,hidden=False):
+        mode = None
         rtn = True
-        if f is None:
-            return False
+        if fname is None:
+            rtn = False
         if not hidden:
-            if f.startswith('.'):
+            if fname.startswith('.'):
                 rtn = False
         if rtn:
-            f = '"{}"'.format(f)
-            cmd = "python -c " + '"import os; os.path.isfile(' +"'"+ f +"'"+ ')"'
-            si,so,se = self._conn.exec_command(cmd)
-            error = se.read().strip()
-            if error == '':
-                res = so.read()
-                if 'True' in res:
-                    if search_files:
-                        rtn = True
-                    else:
-                        rtn = False
-                else:
-                    if search_files:
-                        rtn = False
-                    else:
-                        rtn = True
-            else:
-                raise Exception(error)            
-        return res
+            rtn = self._get_mode(fname)
+        if rtn and rtn != True and stat.ST_ISLNK(rtn):
+            fname = self._get_real_path(fname)
+            rtn = self._get_mode(fname)
+        return rtn
+
+    def is_file(self,f,search_files=True,hidden=False):
+        mode = self._get_real_mode(f,search_files,hidden)
+        return mode is not None and stat.ST_ISREG(mode)
+
+    def is_dir(self,f):
+        mode = self._get_real_mode(f)
+        return mode is not None and stat.ST_ISDIR(mode)
+
 
     def isfile(self,f,*args,**kwargs):
         return self.is_file(f,*args,**kwargs)
         
     def is_dir(self,f):
-        return self.is_file(f,False)
+        return self.is_dir(f)
 
     def isdir(self,f,*args,**kwargs):
-        return self.is_dir(f,*args,**kwargs)
-
+        return self.is_dir(f)
 
     def listdir(self,name='.'):
-        def add_(itm):
-            a,b = itm[0],itm[1]
-            rtn = a + b if a.endswith('/') else a + '/' + b
-            return rtn
-        
+        add_ = lambda itm: os.path.join(itm[0],itm[1])
+        #    #rtn = a + b if a.endswith('/') else os.path.join(a,b)
+        #    #return os.path.join(itm[0],itm[1])                
         if self.is_dir(name):
             res = self._sftp.listdir(name)
             if name != '.':
@@ -115,16 +122,14 @@ class SSHFileBrowser(object):
         data = self.list_dir(dirname)
         files,dirs = [],[]
         for itm in data:
-            tmp = dirname+'/'+itm
+            tmp = os.path.join(dirname,itm)
+            #tmp = dirname+'/'+itm
             if self.is_dir(tmp):
                 dirs.append(itm)
             else:
                 files.append((itm,tmp))
         return files,dirs
-
-        
-
-    
+ 
     def filter_files(files):
         rtn = []
         for f in files:
@@ -160,7 +165,7 @@ class SSHFileBrowser(object):
         return self.ssh.getcwd() or self.base_dir
 
     def dir_name(self,item):
-        cmd = 'python -c "import os; print os.path.dirname(os.path.abspath('+item+'))"'
+        cmd = 'python -c "import os; print os.path.dirname(os.path.abspath('+repr(item)+'))"'
         i,o,e = self.ssh._conn.exec_command(cmd)
         return o.read().strip()
 
@@ -238,11 +243,11 @@ def get_sftp(conn):
     return sftp
 
 
-def process_dir(dirname):
-    return dirname
+def process_dir(dirname,conn):
+    return ('',) if not dirname else (dirname,check_type(conn,dirname)) 
 
-def process_dirs(dirs):
-    return '\n'.join(map(process_dir,dirs))
+def process_dirs(dirs,conn):
+    return '\n'.join(map(str,map(lambda x: process_dir(x,conn),dirs)))
 
 
 def login(user,password,host,**kwargs):
@@ -257,6 +262,53 @@ def _sftp():
     sftp = get_sftp(rtn)
     return sftp
    
+def _get_mode(_conn,fname):
+    cmd = "stat --format=%f {}".format(fname)
+    print 'executing',cmd
+    si,so,se = _conn.exec_command(cmd)
+    data = so.read().strip()
+    print 'got',data
+    result = data and int(data,16)
+    return result
+
+def _is_file(mode):
+    return stat.S_ISREG(mode)
+
+def _is_dir(mode):
+    return stat.S_ISDIR(mode)
+
+def check_type(conn,fname):
+    #print "FNAME:",fname
+    mode = _get_mode(conn,fname)
+    print 'MODE:',mode
+    return mode and ('file' if _is_file(mode) else 'dir') or 'Error'
+
+def _get_real_path(_conn,fname):
+    cmd = 'python -c "import os; print os.path.realpath('+repr(fname)+')"'
+    #print cmd
+    si,so,se = _conn.exec_command(cmd)
+    return so.read()
+
+def dir_name(item,conn):
+    cmd = 'python -c "import os; print os.path.dirname(os.path.abspath('+repr(item)+'))"'
+    i,o,e = conn.exec_command(cmd)
+    return o.read().strip()
+    
+
+def listdir(conn,name='.'):
+    add_ = lambda itm: os.path.join(itm[0],itm[1])
+    #    #rtn = a + b if a.endswith('/') else os.path.join(a,b)
+    #    #return os.path.join(itm[0],itm[1])                
+    if is_dir(name):
+        res = self._sftp.listdir(name)
+        if name != '.':
+            rtn = filter(add_,zip([name]*len(res),res))
+        else:
+            rtn = res
+    else:
+        rtn = name
+    return rtn
+
 
 def main():
     if len(sys.argv) > 1:
@@ -264,11 +316,12 @@ def main():
     else:
         dirname = None
 
-    conn = login()
+    conn = login('root','1414WP8888!!','174.140.227.137')
     sftp = get_sftp(conn)
-    rtn = sftp.listdir() if dirname is None else sftp.listdir(dirname)
-    return process_dirs(rtn)
+    rtn = map(lambda x: os.path.join(dirname or '',x),sftp.listdir() if dirname is None else sftp.listdir(dirname))
+    return '\n'.join(map(str,map(lambda x: check_type(conn,x),map(lambda x: dir_name(x,conn),rtn))))
+    #return process_dirs(rtn,conn)
 
 
 if __name__ == "__main__":
-    main()
+    print main()
